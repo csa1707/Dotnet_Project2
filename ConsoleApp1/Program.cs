39062a0d48eabd87172b4fae5f60a3462891ec89
ASUS <ASUS@DESKTOP-IURUBDV>
2024-01-24 22:04:04 +0530

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,66 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Data;
+using System.Data.SqlClient;
+
+class Program
+{
+
+    static void Main()
+    {
+
+        Console.WriteLine("Hi Ajith , welcome to second project");
+
+        Console.WriteLine("Inserting values into table - vendors");
+
+        var connection = "Data Source= DESKTOP-IURUBDV\\SQLEXPRESS; Initial Catalog=practice;Integrated Security=True";
+
+        var sql = "insert into vendors(eid,vegname,cost) values(@eid,@vegname,@cost)";
+
+        using (SqlConnection conn = new SqlConnection(connection))
+        {
+            conn.Open();
+
+            using (SqlCommand command = new SqlCommand(sql, conn))
+            {
+                command.Parameters.Add("@eid", SqlDbType.Int).Value = 107;
+                command.Parameters.Add("@vegname", SqlDbType.VarChar, 10).Value = "pumpkin";
+                command.Parameters.Add("@cost", SqlDbType.Int).Value = 38;
+
+                command.ExecuteNonQuery();
+            }
+
+            conn.Close();
+
+            Console.WriteLine("Inserting values sucessfully");
+        }
+
+
+
+        Console.WriteLine("deleting values");
+
+        var sqldelete = "delete from vendors where eid=@eid";
+
+        using (SqlConnection conn = new SqlConnection(connection))
+        {
+            conn.Open();
+
+            using (SqlCommand command = new SqlCommand(sqldelete, conn))
+            {
+                command.Parameters.Add("@eid", SqlDbType.Int).Value = 105;             
+
+                command.ExecuteNonQuery();
+            }
+
+            conn.Close();
+
+            Console.WriteLine("deleted values sucessfully");
+        }
+
+
+    }
+
+
+
+}
+
